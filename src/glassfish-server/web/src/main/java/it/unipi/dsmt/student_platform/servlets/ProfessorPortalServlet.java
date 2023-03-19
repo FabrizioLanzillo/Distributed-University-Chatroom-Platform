d5ab5994c2a90cd118230200941415e1b6a41084
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-03-19 12:05:57 +0100

    Professor portal

---
+++ src/glassfish-server/web/src/main/java/it/unipi/dsmt/student_platform/servlets/ProfessorPortalServlet.java
@@ -0,0 +1,28 @@
+package it.unipi.dsmt.student_platform.servlets;
+
+import it.unipi.dsmt.student_platform.enums.UserRole;
+import it.unipi.dsmt.student_platform.utility.AccessController;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ProfessorPortalServlet", value = "/professor/portal")
+public class ProfessorPortalServlet extends HttpServlet {
+	
+	public void doGet (HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException
+	{
+		if (!AccessController.checkAccess(request, response, UserRole.professor)) {
+			return;
+		}
+		
+		// Redirect to portal jsp page
+		request.getRequestDispatcher("/WEB-INF/jsp/professor/portal.jsp")
+				.forward(request, response);
+	}
+	
+}
