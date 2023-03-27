f4bb52c6a712a6b1379ee8109c85b1cf0992326e
FabrizioLanzillo <fabriziolanzillo@gmail.com>
2023-03-27 17:43:11 +0200

    fix of the db

--- src/mysql-server/dsmt_student_platform-dump.sql
+++ src/mysql-server/dsmt_student_platform-dump.sql
@@ -165,11 +165,9 @@ CREATE TABLE `student` (
 
 DROP TABLE IF EXISTS `student_starred_courses`;
 CREATE TABLE `student_starred_courses` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `student` binary(16) NOT NULL,
-  `course` int unsigned,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY (student, course),
+  `course` int unsigned NOT NULL,
+  PRIMARY KEY (student, course),
   FOREIGN KEY (student) REFERENCES student(id),
   FOREIGN KEY (course) REFERENCES course(id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
