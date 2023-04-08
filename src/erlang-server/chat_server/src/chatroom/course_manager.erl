265ba0ebcd28732e82db1e5102eaf94aa7c00959
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-04-08 16:13:42 +0200

    Add modkup for course manager
    Co-Authored-By: Fabrizio Lanzillo <86826030+FabrizioLanzillo@users.noreply.github.com>
    Co-Authored-By: Federico Montini <74728075+FedericoMontini98@users.noreply.github.com>

--- src/erlang-server/chat_server/src/chatroom/course_manager.erl
+++ src/erlang-server/chat_server/src/chatroom/course_manager.erl
@@ -1,1 +1,37 @@
 -module(course_manager).
+-behaviour(gen_server).
+
+%% API
+-include("chat.hrl").
+-export([start_link/0, init/1, handle_call/3, handle_cast/2]).
+
+start_link() ->
+    gen_server:start_link({local, course_manager}, course_manager, [], []).
+
+init(_) ->
+  Sports = ets:new(sports_table, [set]),
+  {ok,Sports}.
+
+handle_call({join_course, Sport, Pid}, _From, Sports) ->
+    Response = check_and_insert_sport(Sport, Pid, Sports),
+    {reply, Response, Sports};
+
+handle_call({get_online_users, Sport}, _From, Sports) ->
+  Response = ets:match_object(Sports, {'_', Sport}),
+  {reply, Response, Sports};
+
+handle_call({logout, Pid}, _From, Sports) ->
+  Response = ets:match_delete(Sports, {Pid, '_'}),
+  {reply, Response, Sports};
+
+handle_call(_Message, _From, State) ->
+  {reply, error, State}.
+
+handle_cast(_Message, State) ->
+  {noreply, State}.
+
+check_and_insert_sport(Sport, Pid, Sports) ->
+  case ets:insert_new(Sports, {Pid, Sport}) of
+    true -> ok;
+    false -> sport_already_inserted
+  end.
