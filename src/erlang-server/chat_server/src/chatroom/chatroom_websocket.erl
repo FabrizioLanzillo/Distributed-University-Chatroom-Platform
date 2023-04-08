5f9b569f502083df1c9a4dad9905113cea3beac9
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-04-08 10:37:15 +0200

    Chatroom html
    - Add html and javascript demo for client browser
    - Add jsone dependency

--- src/erlang-server/chat_server/src/chatroom/chatroom_websocket.erl
+++ src/erlang-server/chat_server/src/chatroom/chatroom_websocket.erl
@@ -12,16 +12,17 @@ init(Req, Opts) ->
     {cowboy_websocket, Req, Opts}.
 
 
-
+% TODO
 % Cowboy will call websocket_handle/2 whenever a text, binary, ping or pong frame arrives from the client.
 websocket_handle(Frame = {text, _Message}, State) ->
+    io:format("Received ~p~n", [Frame]),
     {[Frame], State};
 
 websocket_handle(_Frame, State) ->
     {ok, State}.
 
 
-
+% TODO
 % Cowboy will call websocket_info/2 whenever an Erlang message arrives 
 % (=> from another Erlang process).
 websocket_info({send_message_chatroom, _ServerPid, Msg}, State) ->
@@ -31,7 +32,7 @@ websocket_info(_Info, State) ->
     {ok, State}.
 
 
-
+% TODO
 % Cowboy will call terminate/3 with the reason for the termination of the connection. 
 terminate(_Reason, _Req, _State) ->
     % Logout user from chatroom
