2c666c337b35f958ef035947a4c58383f3965ca5
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-04-20 16:21:49 +0200

    Erlang local configuration

--- src/erlang-server/master_node/run.sh
+++ src/erlang-server/master_node/run.sh
@@ -5,4 +5,4 @@ rm -r Mnesia*
 
 # Start Erlang node. Requires the name of the node as argument of the script 
 # 		=> ./run.sh master@<ip-address>
-rebar3 shell --name $1 --setcookie chat_application
+rebar3 shell --name master@127.0.0.1 --setcookie chat_application
