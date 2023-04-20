9868ab317c7280a10238cda420480cf45dc1439c
Riccardo Sagramoni <50587373+RiccardoSagramoni@users.noreply.github.com>
2023-04-20 16:38:17 +0200

    Erlang remote configuration

--- src/erlang-server/chat_server/run.sh
+++ src/erlang-server/chat_server/run.sh
@@ -5,4 +5,4 @@
 
 # Start Erlang node. Requires the name of the node as argument of the script 
 # 		=> ./run.sh server@<ip-address>
-rebar3 shell --name server@127.0.0.1 --setcookie chat_application
+rebar3 shell --name $1 --setcookie chat_application
--- src/erlang-server/master_node/run.sh
+++ src/erlang-server/master_node/run.sh
@@ -5,4 +5,4 @@
 
 # Start Erlang node. Requires the name of the node as argument of the script 
 # 		=> ./run.sh master@<ip-address>
-rebar3 shell --name master@127.0.0.1 --setcookie chat_application
+rebar3 shell --name master@10.2.1.4 --setcookie chat_application
